966fb410ce6604338f426ceeed2de4b2e4053834
AhmedHamdy434 <cmpunkthebest11@gmail.com>
2026-04-27 16:09:33 +0300

    feat: implement RTL/LTR support using Radix UI Direction and add Vercel routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
 import { AppRouter } from './routes/AppRouter';
+import { useTranslation } from 'react-i18next';
+import { DirectionProvider } from './components/ui/direction';
 
 function App() {
+  const { i18n } = useTranslation();
+  const locale = i18n.language;
+  const direction = locale === 'ar' ? 'rtl' : 'ltr';
   return (
     <QueryClientProvider client={queryClient}>
+      <DirectionProvider dir={direction}>
       <AppRouter />
+      </DirectionProvider>
     </QueryClientProvider>
   );
 }
